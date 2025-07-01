c8a217d30f4d8af743a96d28c5a6c4dcaea9cd97
OnlySarthak <devsarthak.hotline@gmail.com>
2025-07-01 09:44:08 +0530

    playing with the roughts

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test', (req,res)=>{
-    res.send("hello from  test");
+app.get('/get-test', (req,res)=>{
+    res.send("get test");
 });
+app.post('/get-test', (req,res)=>{
+    res.send("post test with get-test");
+});
+app.post('/post-test', (req,res)=>{
+    res.send("post test",req);
+});
+app.use('/get-test', (req,res)=>{
+    res.send("use test with get-test, it will catch the extensions of /get-test at the end of file");
+});
+
 
 app.listen(3000);
